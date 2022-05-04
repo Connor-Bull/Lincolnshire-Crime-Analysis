170c0e5e24d24416e920b81deac2c640de2b548e
BUL19702845 <104578381+Connor-Bull@users.noreply.github.com>
2022-05-05 00:29:44 +0100

    Changes made to map styling

---
+++ static/map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiMTk3MDI4NDUiLCJhIjoiY2wycnk5ZHZkMDBxODNjb2JpNmZubHNqMiJ9.4Fd-52wvMYZgiKLdJr9p0w';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
