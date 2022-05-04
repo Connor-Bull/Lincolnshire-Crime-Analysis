fe049d47e3f5de9c28d8bc591f3c08fba08f2c9b
BUL19702845 <104578381+Connor-Bull@users.noreply.github.com>
2022-05-04 21:22:53 +0100

    map test

---
+++ website/api.py
@@ -0,0 +1,12 @@
+import json
+
+
+from website import app
+from website.data import map as crimeMap
+from website.utils import docache
+
+@app.route('/map', methods=['GET'])
+@docache(minutes=60*24*5) # Cache for 5 days since this will not change!
+def countyNames():
+    return json.dumps(crimeMap)
+
---
+++ website/data.py
@@ -0,0 +1,7 @@
+import json
+
+map = {}
+
+
+with open("data/map.geojson", "r") as mapFile:
+    map = json.load(mapFile)
