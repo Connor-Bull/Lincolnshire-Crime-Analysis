691aadb58360ae38b5e1b4699f0b97c6b17f0211
BUL19702845 <104578381+Connor-Bull@users.noreply.github.com>
2022-05-04 21:52:57 +0100

    map test

--- website/__init__.py
+++ website/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__, static_url_path="/static", static_folder='../static/', template_folder='../templates')
 
+import website.api
+import website.data
 import website.views
