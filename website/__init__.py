cb501e6fa0b2b7497a3368d1dd6e0b5976d4d017
BUL19702845 <104578381+Connor-Bull@users.noreply.github.com>
2022-05-05 14:47:16 +0100

    test

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,5 @@ from flask import Flask
 
 app = Flask(__name__, static_url_path="/static", static_folder='../static/', template_folder='../templates')
 
-import website.api
-import website.data
+
 import website.views
