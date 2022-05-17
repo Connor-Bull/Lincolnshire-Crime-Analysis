94c4a4724dd9b68e15f84261f0d775913209aae6
BUL19702845 <104578381+Connor-Bull@users.noreply.github.com>
2022-05-17 13:42:56 +0100

    Changes to image sliders

--- static/slider0.js
+++ static/slider0.js
@@ -1,6 +1,6 @@
 
- var slides=document.querySelector('.slider-items').children;
- var nextSlide=document.querySelector(".right-slide");
+var slides=document.querySelector('.slider-items').children;
+var nextSlide=document.querySelector(".right-slide");
 var prevSlide=document.querySelector(".left-slide");
 var totalSlides=slides.length;
 var index=0;
